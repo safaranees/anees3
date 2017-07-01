24de1740f395eb5f811e9b4a56f2842c7c00316f
safaranees <aneesparvin08@gmail.com>
2017-07-01 13:38:46 +0530

    Create ani3.c

---
+++ ani3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a;
+clrscr();
+printf("\n enter the character ");
+scanf("\n "%s",a);
+if(a=='a'||a=='e'||a=='i||a=='o'||a==u)
+{
+printf("\n vowel");
+}
+else
+{
+printf("\n consonant");
+}
+getch();
+}
