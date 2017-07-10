1af80d0316e86f713682585b72cd16c00e48f865
safaranees <aneesparvin08@gmail.com>
2017-07-10 12:45:36 +0530

    Create ab1.c

---
+++ ab1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter an character");
+scanf('\n %c",a);
+if((a<0)&&(a>0))
+{
+printf("\n not an alphabet");
+}
+else
+{
+printf('\n alphabet");
+}
+getch();
+]
