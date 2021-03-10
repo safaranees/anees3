d700c8dc8897d2eca642bf187cc7ded847c82e6e
safaranees <aneesparvin08@gmail.com>
2021-03-10 11:44:48 +0530

    Add files via upload

---
+++ Springboot-jpa/src/main/java/com/hcl/SpringbootJpa.java
@@ -0,0 +1,14 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootJpa {
+public static void main(String[] args){
+		
+		SpringApplication.run(SpringbootJpa.class, args);
+		
+	}
+}
